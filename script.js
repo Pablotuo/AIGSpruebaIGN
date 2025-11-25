31b87cbb9b75c1b297e53abc0cee8cca521ab66a
Pablotuo <43593145+Pablotuo@users.noreply.github.com>
2025-11-25 17:38:00 -0600

    Refactor comments and update location descriptions

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // Crear la variable map, crea un mapa y lo muestra en las coordenadas(lat,Lon) con un zoom determinado(8).
 var map = L.map('map').setView([9.7489, -83.7534], 8);
-// Activa capabase de OpenStreetMap 
+// Activa capabase de OpenStreetMap
 var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors' //Esto ultimo no se que es 
 });
-
 // Activa capabase Satélite ESRI
 var satellite = L.tileLayer(
     'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
@@ -13,22 +12,21 @@ var satellite = L.tileLayer(
         attribution: '© ESRI World Imagery'
     }
 );
-
-// Mostrar OSM por defecto
+// Mostrar OpenStreetMaps por defecto
 street.addTo(map);
 
-// Control de capas
+// Lo que muestra el boton de seleccion de mapas.
 var baseMaps = {
-    "Mapa Claro (OSM)": street,
+    "Mapa": street,
     "Satélite": satellite
 };
-
+//Crea el boton de seleccion de mapas. 
 L.control.layers(baseMaps).addTo(map);
 
-// Lista de lugares con latitudes, longitudes, nombres, descripciones y la imagen
+// Lista de Puntos de Interes, crea la variable lugares
 var lugares = [ 
   {
-    lat: 9.992554,
+    lat: 9.992554,   
     lon: -83.024251,
     nombre: "57-LA",
     descripcion: "Blanco",
@@ -38,7 +36,7 @@ var lugares = [
     lat: 10.992554,
     lon: -84.024251,
     nombre: "57-LB",
-    descripcion: "Blanco",
+    descripcion: "Amarillo",
     imagen: "https://github.com/Pablotuo/ChichiHitos/blob/main/images/20220501_122303.jpg?raw=true"
   }
 ];
